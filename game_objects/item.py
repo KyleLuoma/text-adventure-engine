c75424cb03ffffab2cac287dc3bd7912d1ed909a
kyleluoma <kyle@jp8.dev>
2023-08-11 21:39:36 -0700

    added item existence in scenes

--- game_objects/item.py
+++ game_objects/item.py
@@ -1,5 +1,15 @@
+import json
 
 class Item:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, item_id) -> None:
+        self.id = item_id
+        item_dict = self._load_item(item_id)
+        self.name = item_dict['name']
+        self.description = item_dict['description']
+
+
+    def _load_item(self, item_id) -> dict:
+        with open("./assets/items/" + str(item_id) + ".json", "r") as file:
+            item_dict = json.load(file)
+        return item_dict
--- game_objects/scene.py
+++ game_objects/scene.py
@@ -1,11 +1,16 @@
-
 class Scene:
+
+    from .item import Item
 
     def __init__(self, scene_dict) -> None:
         self.id = scene_dict['id']
         self.title = scene_dict['title']
         self.full_description = scene_dict['full_description']
         self.paths = scene_dict['paths']
+        items = scene_dict['items']
+        self.items = []
+        for item in items:
+            self.items.append(self.Item(item['item_type_id']))
         self.path_lookup = {}
         for path in self.paths:
             self.path_lookup[path['direction_strict']] = path
@@ -20,6 +25,9 @@ class Scene:
         for path in self.paths:
             description_string += path['direction_description'] + " "
             description_string += path['description'] + " "
+        for item in self.items:
+            description_string += "There is a "
+            description_string += item.name + " "
         return description_string
     
     def get_valid_commands(self) -> str:
